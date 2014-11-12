489de7b19732abfb2a6cb98880e5aa5b4d0ca094
gmetaxas <gmetaxas@globoplc.com>
2014-11-12 16:42:54 +0200

    Changed makefile to also generate shared library

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,8 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := coffeecatch
+LOCAL_MODULE := coffeecatch_static
 LOCAL_SRC_FILES += ../coffeecatch.c ../coffeejni.c
 LOCAL_CFLAGS := -funwind-tables -Wl,--no-merge-exidx-entries
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := coffeecatch
+LOCAL_STATIC_LIBRARIES := coffeecatch_static
+LOCAL_CFLAGS := -funwind-tables -Wl,--no-merge-exidx-entries
+
+include $(BUILD_SHARED_LIBRARY)
