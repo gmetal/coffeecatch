14ca18fb3a21b1a6a1aace66f9a30cca2b4d72c3
gmetaxas <gmetaxas@globoplc.com>
2014-11-12 16:37:59 +0200

    Added android makefiles Fixed android compilation errors.
    Support for arm64 is not available yet

---
+++ jni/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := coffeecatch
+LOCAL_SRC_FILES += ../coffeecatch.c ../coffeejni.c
+LOCAL_CFLAGS := -funwind-tables -Wl,--no-merge-exidx-entries
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ jni/Application.mk
@@ -0,0 +1,4 @@
+APP_ABI := armeabi armeabi-v7a x86 x86_64
+NDK_TOOLCHAIN_VERSION := clang
+APP_CPPFLAGS += -std=c++11
+APP_PLATFORM := android-14
