dd6d984c86f731e05fbc07ee07f9039f8359f491
gmetaxas <gmetaxas@globoplc.com>
2014-11-30 19:24:33 +0200

    Fixed makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := coffeecatch_static
-LOCAL_SRC_FILES += ../coffeecatch.c ../coffeejni.c
+LOCAL_SRC_FILES := ../coffeecatch.c ../coffeejni.c
 LOCAL_CFLAGS := -funwind-tables -Wl,--no-merge-exidx-entries
 
 include $(BUILD_STATIC_LIBRARY)
@@ -11,7 +11,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := coffeecatch
-LOCAL_STATIC_LIBRARIES := coffeecatch_static
+LOCAL_SRC_FILES := ../coffeecatch.c ../coffeejni.c
 LOCAL_CFLAGS := -funwind-tables -Wl,--no-merge-exidx-entries
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi armeabi-v7a x86 x86_64
+APP_ABI := armeabi armeabi-v7a x86 x86_64 arm64-v8a
 NDK_TOOLCHAIN_VERSION := clang
 APP_CPPFLAGS += -std=c++11
 APP_PLATFORM := android-14
